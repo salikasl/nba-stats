2441f5717f067a2dfee6afc6435aca9f746af6fc
shayaand <sdar@umich.edu>
2020-12-10 13:17:03 -0500

    pull

--- ncaa.py
+++ ncaa.py
@@ -2,9 +2,10 @@ import requests
 import json
 import os
 import sqlite3
-from sportsreference.ncaab.roster import Player
+import plotly.express as px
+import pandas as pd 
+from sportsreference.ncaab.roster import Player, Roster
 from sportsreference.ncaab.teams import Teams
-from sportsreference.ncaab.roster import Roster
 from sportsreference.ncaab.rankings import Rankings
 
 
@@ -24,7 +25,7 @@ def setUpDatabase(db_name):
 
 def setUpTable(cur,conn):
     cur.execute("DROP TABLE IF EXISTS NCAA")
-    cur.execute("CREATE TABLE NCAA (name TEXT, id TEXT, season STRING, points FLOAT, assists FLOAT, rebounds FLOAT, blocks FLOAT, steals FLOAT, field_goal_percentage FLOAT, three_point_percentage FLOAT, minutes_played INTEGER)")
+    cur.execute("CREATE TABLE NCAA (name TEXT, id TEXT, season STRING, total_points INTEGER, points FLOAT, assists FLOAT, rebounds FLOAT, blocks FLOAT, steals FLOAT, field_goal_percentage FLOAT, three_point_percentage FLOAT, minutes_played INTEGER, points_per_minute FLOAT)")
 
 def getPlayers(cur,conn):
     cur.execute("SELECT name FROM players")
@@ -32,7 +33,7 @@ def getPlayers(cur,conn):
     name_list = [x[0] for x in NBA_names]
     schools = getSchools()
     shared_names = []
-    seasons = ['2011','2012','2013','2014','2015','2016']
+    seasons = ['2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016']
     for team in schools:
             for x in seasons: 
                 roster = Roster(team,x,False)
@@ -50,6 +51,8 @@ def insertNCAAstats(cur,conn):
     names = getPlayers(cur,conn)
     for name,ids,season in names:
         season = season[:3]+(str(int(season[3])-1))+'-'+(season[2:])
+        if season == '201-1-10':
+            season=='2009-10'
         player = Player(ids)
         games_played = player(season).games_played 
         assist = player(season).assists/games_played
@@ -57,17 +60,23 @@ def insertNCAAstats(cur,conn):
         block = player(season).blocks/games_played
         fg_percentage = player(season).field_goal_percentage
         minutes = player(season).minutes_played
+        total_points = player(season).points
         point = player(season).points/games_played
         steal = player(season).steals/games_played
-        three_point_perc = player(season).three_point_percentage                  
-        cur.execute("INSERT INTO NCAA (name, id, season, points, assists, rebounds, blocks, steals, field_goal_percentage, three_point_percentage, minutes_played) VALUES (?,?,?,?,?,?,?,?,?,?,?)",(name,ids,season,point,assist,rebound,block,steal,fg_percentage,three_point_perc,minutes,))
+        three_point_perc = player(season).three_point_percentage 
+        points_per_minute = player(season).points/minutes                 
+        cur.execute("INSERT INTO NCAA (name, id, season, total_points, points, assists, rebounds, blocks, steals, field_goal_percentage, three_point_percentage, minutes_played,points_per_minute) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)",(name,ids,season,total_points,point,assist,rebound,block,steal,fg_percentage,three_point_perc,minutes,points_per_minute,))
         conn.commit()
-
-
+    
+def visualize(cur,conn):
+    df = pd.read_sql_query("SELECT * FROM NCAA",conn)
+    fig = px.line(df,x='season',y='points',color='name')
+    fig.show()
 
 def main():
     cur,conn = setUpDatabase('stats.db')
     setUpTable(cur,conn)
     insertNCAAstats(cur,conn)
+    #visualize(cur,conn)
 
 main()
